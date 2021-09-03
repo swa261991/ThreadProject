ecefb84a03b114677ef266c315d00cf3a6d9041c
Swapnil <swapniljadhav261991@gmail.com>
2021-09-04 04:31:14 +0530

    StreamExample1

--- src/main/java/com/JavaStreams/StreamExample1.java
+++ src/main/java/com/JavaStreams/StreamExample1.java
@@ -1,7 +1,9 @@
 package com.JavaStreams;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class StreamExample1 {
     public static void main(String[] args) {
@@ -15,5 +17,60 @@ public class StreamExample1 {
         IntStream.range(0,10).filter(x->x>4).forEach(x-> System.out.print(x+" "));
         System.out.println();
         IntStream.range(0,10).filter(x->x%2==0).forEach(x-> System.out.print(x+" "));
+        System.out.print("\n");
+        String[] names = {"Adam", "Daniel", "Martha", "Kevin", "Ben", "Joe", "Brad", "Susan"};
+        Stream.of(names).forEach(System.out::println);
+        System.out.println("-------- sorted asc ----------");
+        Stream.of(names).sorted().forEach(System.out::println);
+        System.out.println("-------- sorted desc ----------");
+        Stream.of(names).sorted(Comparator.reverseOrder()).forEach(System.out::println);
+        System.out.println("-------- starts with ----------");
+        Stream.of(names).filter(x-> x.startsWith("A")).forEach(System.out::println);
     }
 }
+
+/*
+OUTPUT
+------
+
+1
+5
+3
+-2
+9
+12
+Sum: 28
+0 1 2 3 4
+5 6 7 8 9
+0 2 4 6 8
+Adam
+Daniel
+Martha
+Kevin
+Ben
+Joe
+Brad
+Susan
+-------- sorted asc ----------
+Adam
+Ben
+Brad
+Daniel
+Joe
+Kevin
+Martha
+Susan
+-------- sorted desc ----------
+Susan
+Martha
+Kevin
+Joe
+Daniel
+Brad
+Ben
+Adam
+-------- starts with ----------
+Adam
+
+
+* */
